dda1b025d7d64f9c30d74b956f9193a1b7f9dc47
Azimbek Alibekov <atalgat592@gmail.com>
2025-09-03 22:47:43 +0500

    sdf

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,23 +1,22 @@
 import openai from "../utils/clientAi.js"
 
+const chatH = [];
 export const chat_controller = async (req, res) => {
-
 
     try {
         const { mode, prompt } = req.body;
+        console.log(mode, prompt)
 
         chatH.push({ role: "user", content: prompt });
-        
+
         let output;
 
-        if (mode === 'chat') {
-            output = await openai.chat.completions.create({
-                model: "gpt-4o-mini",
-                messages: chatH
-            });
-        }
+        output = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: chatH
+        });
 
-        res.json({ mode, output1: output.choices[0].message.content });
+        res.send({ mode, output: output.choices[0].message.content });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Internal server error" });
